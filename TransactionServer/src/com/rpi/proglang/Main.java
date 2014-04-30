e6ae86ff8cdb74a6cbf2d7412b89337af08c499f
Kevin Sullivan <sideslapd@gmail.com>
2014-04-30 16:01:39 -0400

    Reoganized Properly

--- TransactionServer/src/com/rpi/proglang/Main.java
+++ TransactionServer/src/com/rpi/proglang/Main.java
@@ -1,8 +1,13 @@
 package com.rpi.proglang;
 
+import com.rpi.proglang.server.Server;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(args[0]);
+
+        Server test = new Server();
 	// write your code here
     }
 }
