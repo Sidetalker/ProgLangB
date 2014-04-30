51bcd623eea6ae57b266f6060b52803167986c2e
Kevin Sullivan <sideslapd@gmail.com>
2014-04-30 16:28:22 -0400

    Working non-threaded

--- TransactionServer/src/com/rpi/proglang/Main.java
+++ TransactionServer/src/com/rpi/proglang/Main.java
@@ -2,12 +2,17 @@ package com.rpi.proglang;
 
 import com.rpi.proglang.server.Server;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        throws IOException {
+            // Gotta catch em all
+
         System.out.println(args[0]);
 
-        Server test = new Server();
+        Server.main(args);
 	// write your code here
     }
 }
